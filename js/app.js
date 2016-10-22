0992ed6f986b2b13390d87c3fa5bbb547b03db99
Petar Ivanov <psivanov@gmail.com>
2016-10-22 02:49:12 -0700

    hook contribution updates to the backend

--- js/app.js
+++ js/app.js
@@ -15,7 +15,7 @@ var CampaignList = React.createClass({
 						${rootClass}-${(item.amount < item.targetAmount ? 'unmet' : 'met')}
 						${this.props.selected === item ? rootClass+'-selected' : ''}`}
 					key = { index }
-					onClick = { () => { this.props.onSelect(item) } }  
+					onClick = { () => { this.props.onSelect(item['.key']) } }  
 				>
 					<div className={`${rootClass}-header`}>
 						<div className={`${rootClass}-title`}>{ item.title } </div>
@@ -26,7 +26,6 @@ var CampaignList = React.createClass({
 			);
 		};
 		return <div>
-			<h2>Кампании</h2>
 			{ this.props.items.map(createItem) }
 		</div>
 	  }
@@ -42,7 +41,7 @@ var InitiativeList = React.createClass({
 						${rootClass}-${(item.amount.converted < item.targetAmount ? 'unmet' : 'met')}
 						${this.props.selected === item ? rootClass+'-selected' : ''}`}
 					key = { index }
-					onClick = { () => { this.props.onSelect(item) } }  
+					onClick = { () => { this.props.onSelect(item['.key']) } }  
 				>
 					<div className={`${rootClass}-header`}>
 						<div className={`${rootClass}-title`}>{ item.title } </div>
@@ -53,7 +52,6 @@ var InitiativeList = React.createClass({
 			);
 		};
 		return <div>
-			<h2>Инициативи</h2>
 			{ this.props.items && this.props.items.map(createItem) }
 		</div>
 	  }
@@ -82,32 +80,79 @@ var App = React.createClass({
 		});	
 	},
 
-	getInitiativeAmount: function(currency) {
-		var dict = this.state.selectedInitiative.amount[currency];
+	getInitiativeAmount: function(initiative, currency) {
+		var dict = initiative && initiative.amount[currency];
 		return (dict && dict[this.state.user.uid]) || 0;
 	},
-	
-	onChange: function(e) {
-		this.setState({text: e.target.value});
-	},
 
+	getSelectedCampaign: function() {
+		return this.state.campaign && this.state.campaigns.find(x => x['.key'] === this.state.selectedCampaign);
+	},	
+	getSelectedInitiative: function() {
+		return this.state.initiatives && this.state.initiatives.find(x => x['.key'] === this.state.selectedInitiative);
+	},
+	
+	handleContributionChange: function(e, currency) {
+		var contribution = this.state.contribution;
+		contribution[currency] = e.target.value;
+		this.setState({contribution});
+	},
+	
 	handleSelectCampaign: function(campaign) {
-		if((this.state.selectedCampaign && this.state.selectedCampaign['.key']) !== (campaign && campaign['.key'])) {				
+		if(this.state.selectedCampaign !== campaign) {
 			this.setState({
 				selectedCampaign: campaign,
 				selectedInitiative: null
-			});	
-			
-			var firInitiativesRef = firebase.database().ref(`initiatives/${campaign['.key']}`);
+			});
+
+			//initiatives
+			var firInitiativesRef = firebase.database().ref(`initiatives/${campaign}`);
 			if (this.state.initiatives)
 				this.unbind('initiatives');
 			this.bindAsArray(firInitiativesRef, 'initiatives');
 		}	
 	},
 	handleSelectInitiative: function(initiative) {
-		if((this.state.selectedInitiative && this.state.selectedInitiative['.key']) !== (initiative && initiative['.key'])) {				
+		if(this.state.selectedInitiative !== initiative) {
 			this.setState({selectedInitiative: initiative});
+			this.syncContribution(this.state.initiatives.find(x => x['.key'] === initiative));
 		}	
+	},
+	handleUpdateContribution: function() {
+		var bgnIncrement = this.state.contribution.BGN - this.getInitiativeAmount(this.getSelectedInitiative(), 'BGN');
+		var eurIncrement = this.state.contribution.EUR - this.getInitiativeAmount(this.getSelectedInitiative(), 'EUR');
+		var usdIncrement = this.state.contribution.USD - this.getInitiativeAmount(this.getSelectedInitiative(), 'USD');
+		var converted = bgnIncrement + convertCurrency(eurIncrement, 'EUR', 'BGN') + convertCurrency(usdIncrement, 'USD', 'BGN');
+		//get user id
+		var uid = this.state.user.uid;
+		//get ref to initiative amount
+		var amountRef = firebase.database().ref(`initiatives/${this.state.selectedCampaign}/${this.state.selectedInitiative}/amount`);
+		//getRef to campaign
+		var campaignRef = firebase.database().ref(`campaigns/${this.state.selectedCampaign}/amount`);	
+		if(bgnIncrement !== 0)
+			amountRef.child(`BGN/${uid}`).transaction(currentData => this.firAdd(currentData, bgnIncrement));
+		if(eurIncrement !== 0)
+			amountRef.child(`EUR/${uid}`).transaction(currentData => this.firAdd(currentData, eurIncrement));
+		if(usdIncrement !== 0)
+			amountRef.child(`USD/${uid}`).transaction(currentData => this.firAdd(currentData, usdIncrement));
+		if(converted !== 0) {
+			amountRef.child('converted').transaction(currentData => this.firAdd(currentData, converted));
+			campaignRef.transaction(currentData => this.firAdd(currentData, converted));
+		}			
+	},
+	firAdd: function(currentData, increment) {
+		if(!currentData)
+			currentData = 0;
+		return currentData + increment;
+	},
+	syncContribution: function(initiativeObj) {
+		this.setState({
+			contribution: {
+				BGN: this.getInitiativeAmount(initiativeObj, 'BGN'),
+				EUR: this.getInitiativeAmount(initiativeObj, 'EUR'),
+				USD: this.getInitiativeAmount(initiativeObj, 'USD')
+			}
+		});
 	},
 
   render: function() {
@@ -117,19 +162,19 @@ var App = React.createClass({
 			{this.state.user &&
 				<div className='main'>
 					<div className='top'>
-						<div className='left'>{<CampaignList items={ this.state.campaigns } selected={this.state.selectedCampaign} onSelect={ this.handleSelectCampaign }/>}</div>
-						<div className='center'/>
-						<div className='right'>{this.state.selectedCampaign && <InitiativeList items={ this.state.initiatives } selected={this.state.selectedInitiative} onSelect={ this.handleSelectInitiative }/>}</div>
+						<div className='left'><h2>Кампании</h2>{<CampaignList items={ this.state.campaigns } selected={this.getSelectedCampaign()} onSelect={ this.handleSelectCampaign }/>}</div>
+						<div className='center'><hr style={{width:'1px', height:'100%'}}/></div>
+						<div className='right'><h2>Инициативи</h2>{this.state.selectedCampaign && <InitiativeList items={ this.state.initiatives } selected={this.getSelectedInitiative()} onSelect={ this.handleSelectInitiative }/>}</div>
 					</div>
 					<div className='bottom'>
 						<hr className='divider'/>
 						{this.state.selectedInitiative && 
 							<div className='contribution'>
 								<b>Моя принос:</b>
-								<div>BGN: <input value={this.getInitiativeAmount('BGN')}/></div>
-								<div>EUR: <input value={this.getInitiativeAmount('EUR')}/></div>
-								<div>USD: <input value={this.getInitiativeAmount('USD')}/></div>
-								<button>Запази</button>
+								<div>BGN: <input value={this.state.contribution.BGN} onChange={(e) => this.handleContributionChange(e, 'BGN')}/></div>
+								<div>EUR: <input value={this.state.contribution.EUR} onChange={(e) => this.handleContributionChange(e, 'EUR')}/></div>
+								<div>USD: <input value={this.state.contribution.USD} onChange={(e) => this.handleContributionChange(e, 'USD')}/></div>
+								<button onClick={this.handleUpdateContribution}>Запази</button>
 							</div>
 						}
 					</div>	
